0a263ce060d8be73b9d7ce8b2d05136dd4140deb
Hua Yanghao <yanghao@users.noreply.github.com>
2021-05-24 22:49:36 +0200

    Update super_car.py
    line searching and following works now.

--- super_car.py
+++ super_car.py
@@ -1,22 +1,58 @@
-# Add your Python code here. E.g.
 from microbit import *
 from tinybit import *
 from random import randint
 
+FIND_LINE = False
+
 def sleep_run():
-    pass
+    global FIND_LINE
+    FIND_LINE = False
+    music.stop()
+    run(0, 0)
+    light(0, 0, 0, 0)
+    light(1, 0, 0, 0)
 
 def sonar_run(speed=80):
     d = measure()
+    light(0, 0, 0, 0)
+    light(1, 0, 0, 0)
     if d < 30:
+        light(1, 100, 0, 0)
         run(-speed, -speed)
     elif d > 50:
+        light(0, 50, 50, 50)
         run(speed, speed)
     else:
+        light(0, 100, 0, 0)
         run(0, 0)
 
-def line_run(speed=90):
-    pass
+def line_run(speed=60, delta=60):
+    global FIND_LINE
+    left, right = line()
+    if FIND_LINE == True:
+        light(0, 30, 30, 30)
+        light(1, 0, 0, 0)
+        if left == 1 and right == 1:
+           light(0, 30, 0, 0)
+           run(speed,-speed)
+        elif left == 1 and right == 0:
+            light(0, 0, 30, 0)
+            run(speed-delta,speed)
+        elif left == 0 and right == 1:
+            light(0, 0, 0, 30)
+            run(speed,speed-delta)
+        elif left == 0 and right == 0:
+            a = randint(0, 50)
+            b = randint(0, 50)
+            c = randint(0, 50)
+            light(1, a, b, c)
+            light(0, c, a, b)
+            run(speed, speed)
+    else:
+        light(0, 50, 0, 0) # searching for line
+        run(speed, speed)
+        if left == 1 or right == 1:
+            FIND_LINE = True
 
 def radio_run():
     pass
@@ -29,7 +65,6 @@ def check():
         sleep(1000)
 
 def party_run():
-    #display.scroll("Partymodus") # Aufgabe 1
     max_num = 50
     check()
     music.play(music.PYTHON, wait=False)
@@ -50,21 +85,22 @@ def party_run():
 
 # button_a: next mode, button_b: sleep mode
 def super_run():
+    # dual mode
+    try:
+        run(0,0)
+    except: # I2C error ...
     run_list = [sleep_run, sonar_run, line_run, radio_run, party_run]
     mode = 0
     while True:
         display.show(mode)
-        run(0, 0)
-        music.stop()
-        light(0, 0, 0, 0)
-        light(1, 0, 0, 0)
+        sleep_run()
         func = run_list[mode]
         while True:
             func()
-            if button_a.is_pressed():
+            if button_a.is_pressed():# or (pin1.read_analog() > 200):
                 mode = mode + 1
                 mode = mode % len(run_list)
-                sleep(1000)
+                sleep(500)
                 break # exit the current loop: inner loop --> return to outer loop
             if button_b.is_pressed():
                 mode = 0
