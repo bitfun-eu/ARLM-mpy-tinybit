145eecb60bd8fd4056726f8eb41a2a72c07e3f42
Hua Yanghao <yanghao@users.noreply.github.com>
2021-09-24 22:27:06 +0200

    Create voice_control.py

---
+++ voice_control.py
@@ -0,0 +1,20 @@
+# Add your Python code here. E.g.
+from microbit import pin1, sleep
+from tinybit import run
+
+def listen(level=100, duration_ms=1000, speed=70, wait=300):
+    noise = pin1.read_analog()
+    if noise > level:
+        run(speed, speed)
+        sleep(duration_ms)
+    run(0, 0)
+    sleep(wait)
+
+while True:
+    listen()
+
+# Questions
+# 1. Why do we sleep(wait) at line 11? what could happen if we don't do that?
+# 2. Can we use two microphone, such that tinybit can run towards the voice source?
+# e.g. if voice comes from left, then it turns left, if voice comes from right, it turns right?
+# There is a mic sensor in the hardware pack, please use microbit v1 if your tinybit is not fixed for v2.
