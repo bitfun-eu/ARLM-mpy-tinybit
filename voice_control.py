ef90246e379e9133a4caefa617a6113d1a9b213e
Hua Yanghao <yanghao@users.noreply.github.com>
2021-09-25 14:38:54 +0200

    Update voice_control.py

--- voice_control.py
+++ voice_control.py
@@ -18,3 +18,20 @@ while True:
 # 2. Can we use two microphone, such that tinybit can run towards the voice source?
 # e.g. if voice comes from left, then it turns left, if voice comes from right, it turns right?
 # There is a mic sensor in the hardware pack, please use microbit v1 if your tinybit is not fixed for v2.
+
+# Below is another version which is more sensitive, think about why this is the case?
+from microbit import pin1, sleep
+from tinybit import run
+
+def listen(level=100, duration_ms=1000, speed=70, wait=300):
+    while True:
+        noise = pin1.read_analog()
+        if noise < level:
+            continue
+        else:
+            run(speed, speed)
+            sleep(duration_ms)
+        run(0, 0)
+        sleep(wait)
+
+listen()
